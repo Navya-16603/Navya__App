45b3f0ba75d049ed50c2c40b9ce96ccb09f7d8c8
Navya-16603 <navyaakkineni44@gmail.com>
2026-04-01 12:21:32 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-# Navigate to the application directory
 cd /home/ec2-user/myapp
 
-# Stop old PM2 processes safely
 pm2 stop all || true
 
-# Install dependencies
 npm install
 
-# Start the application with PM2
 pm2 start app.js --name myapp
 
-# Save the PM2 process list
 pm2 save
