febfdcf0c601e51458b7eae927ae7cda360dfb44
Navya-16603 <navyaakkineni44@gmail.com>
2026-04-02 12:17:54 +0530

    Delete scripts directory

--- scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Clean old application files
-
-echo "Cleaning old application files..."
-
-rm -rf /home/ec2-user/myapp/*
-echo "Cleanup completed."
-``
--- scripts/scripts/stop_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Stopping existing application..."
-pkill -f app.py || true
-pkill -f app.js || true
