1bc749bba78ab02f9ae7fc67c0b90f803112d725
Navya-16603 <navyaakkineni44@gmail.com>
2026-04-08 17:15:32 +0530

    Delete scripts directory

--- scripts/start_server.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Starting Node.js Application..."
-cd /var/www/app
-pkill -f "node app.js" || true
-nohup node app.js > node_output.log 2>&1 &
--- scripts/stop_server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Stopping Node.js application..."
-pkill -f "node app.js" || true
-pkill -f node || true
